e2fe3b7487f2ee112d7a83eb51b324342f2dadc3
James Rostirolla <j_rostirolla@hotmail.com>
2021-10-07 12:11:33 +1100

    issues with git

---
+++ public/service-worker.js
@@ -0,0 +1,11 @@
+/* 
+TODO: 
+    - add in caching elements
+    - analyse exercise 22 for the install / caching functions
+    - 
+*/
+
+const FILES_TO_CACHE = [
+    '/',
+    
+]
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+    mode: "production",
+    entry: {
+        index: "./public/index.html",
+    },
+    output: {
+        path: __dirname = "/public/dist",
+        filename: "[name].bundle.js"
+    },
+    plugins: [
+        new WebpackPwaManifest({
+        //configuration
+            filename: "manifest.json",
+            inject: false,
+            fingerprints: false,
+        //contents
+            name:"PWA-Budget-Tracker",
+            short_name: "budgetTracker",
+            theme_color: "#ffffff",
+            background_color: "#ffffff",
+            start_url:"/",
+            display: "standalone",
+        //icon setup
+            icons: [
+                {
+                    src: path.resolve(__dirname,"public/icons/icon-192x192.png"),
+                    size: [192, 512],
+                    destination: path.join("icons")
+                }
+            ]
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.m?js$/,
+            exclude: /(node_modules)/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env"]
+                }
+            }
+        }]
+    }
+}
+
+module.exports = config;
