ae04b6cb90d4dfe2e5d7410625b783657829f060
James Rostirolla <j_rostirolla@hotmail.com>
2021-10-10 12:36:26 +1100

    fixed reader issue, issue with file not writing

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,3 @@
-/* 
-TODO: 
-    - add in caching elements
-    - analyse exercise 22 for the install / caching functions
-    - 
-*/
-
 const FILES_TO_CACHE = [
     '/',
     "/index",
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require("path");
 const config = {
     mode: "production",
     entry: {
-        index: "./public/index.html",
+        index: "./public/index.js",
     },
     output: {
         path: __dirname = "/public/dist",
@@ -13,38 +13,40 @@ const config = {
     plugins: [
         new WebpackPwaManifest({
         //configuration
-            filename: "manifest.json",
+            // filename: "manifest.json",
             inject: false,
             fingerprints: false,
         //contents
             name:"PWA-Budget-Tracker",
             short_name: "budgetTracker",
+            description: "An Offline / online budget tracker",
             theme_color: "#ffffff",
             background_color: "#ffffff",
             start_url:"/",
-            display: "standalone",
+            // display: "standalone",
         //icon setup
             icons: [
                 {
-                    src: path.resolve(__dirname,"public/icons/icon-192x192.png"),
+                    src: path.resolve("public/icons/icon-192x192.png"),
                     size: [192, 512],
-                    destination: path.join("icons")
-                }
-            ]
+                    // destination: path.join("icons")
+                }]
         })
     ],
     module: {
-        rules: [{
+        rules: [
+          {
             test: /\.m?js$/,
             exclude: /(node_modules)/,
             use: {
-                loader: "babel-loader",
-                options: {
-                    presets: ["@babel/preset-env"]
-                }
+              loader: "babel-loader",
+              options: {
+                presets: ["@babel/preset-env"]
+              }
             }
-        }]
-    }
-}
+          }
+        ]
+      }
+    };
 
 module.exports = config;
